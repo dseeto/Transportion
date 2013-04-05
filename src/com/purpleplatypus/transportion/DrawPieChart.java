7c38d37f2dab96cd3f500c736e8b8203f07c67f6
Tiffany Lee <tiffanylee@Tiffany-Lees-MacBook.local>
2013-04-05 16:18:58 -0700

    Added DrawPieChart; changed MainActivity.java to add piechart; changed mainActivity.xml

---
+++ src/com/purpleplatypus/transportion/DrawPieChart.java
@@ -0,0 +1,54 @@
+package com.purpleplatypus.transportion;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Paint.Style;
+import android.graphics.RectF;
+import android.view.View;
+
+public class DrawPieChart extends View{
+
+	Paint p = new Paint(Paint.ANTI_ALIAS_FLAG);	
+	float[] valueDegrees;
+	int c[] = { Color.RED, Color.YELLOW, Color.BLUE, Color.GREEN};
+
+	
+	public DrawPieChart(Context context, float[] data) {
+		super(context);
+		// hardcode the piechart:
+		valueDegrees = data;
+	}	
+	
+	public void draw(Canvas canvas) {
+	    int x = getWidth();
+	    int y = getHeight();	    
+	    // CHANGE THESE SETTINGS
+	    p.setColor(Color.parseColor("#78777D"));
+	    p.setStyle(Style.STROKE);
+	    p.setStrokeWidth(2);
+	    canvas.drawRect(0, 0, x - 1, y - 1, p);	    	    
+	    float curPos = -90;
+	    p.setStyle(Style.FILL);
+	    RectF rect = new RectF(20, 20, x - 20, y - 20);
+	    // CHANGE THESE SETTINGS	    	    
+	    
+	    for (int i = 0; i < 4; i++) {
+	    	p.setColor(c[i]);
+	    	curPos = curPos + valueDegrees[i];
+	    	canvas.drawArc(rect, curPos, valueDegrees[i], true, p);
+	    }
+	    
+	    /* FOR REFERENCE:	   
+	    for (int i = 0; i < 4; i++) {
+	        p.setColor(c[i]);
+	        float thita = (t == 0) ? 0 : 360 * aList.get(i) / t;
+	        canvas.drawArc(rect, curPos, thita, true, p);
+	        curPos = curPos + thita;
+	    }
+	    */
+	}
+
+	
+}
--- src/com/purpleplatypus/transportion/MainActivity.java
+++ src/com/purpleplatypus/transportion/MainActivity.java
@@ -3,13 +3,32 @@ package com.purpleplatypus.transportion;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.widget.LinearLayout;
 
 public class MainActivity extends Activity {
-
+	
+	LinearLayout pieChartLayout;
+	DrawPieChart pieChart;
+	float[]chartValues = new float[4];
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		pieChartLayout = (LinearLayout) findViewById(R.id.mainPieChart);
+		
+		//hardcode values for pie chart:
+		//order: car, bus, bike, walk
+	    chartValues[0] = 100;
+	    chartValues[1] = 500;
+	    chartValues[2] = 0;
+	    chartValues[3] = 1000;
+	    
+	    pieChart = new DrawPieChart(this, chartValues);
+	    pieChartLayout.addView(pieChart);
+
 	}
 
 	@Override
@@ -18,5 +37,24 @@ public class MainActivity extends Activity {
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
+	
+	public void onClick(View v) {
+		switch (v.getId()) {
+		case R.id.car:
+			
+			break;
+		case R.id.bus:
+			
+			break;
+		case R.id.bike:
+			
+			break;
+		case R.id.walk:
+			
+			break;
+		}
+	}
+	
+	
 
 }
