291757abf61217e9849af7e05255c4a9770b7432
haotian wang <haotianwang92@gmail.com>
2013-04-06 17:27:20 -0700

    added TransportionActivity and FriendsActivity with associated layouts

---
+++ src/com/purpleplatypus/transportion/FriendsActivity.java
@@ -0,0 +1,28 @@
+package com.purpleplatypus.transportion;
+
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class FriendsActivity extends TransportionActivity {
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		// special TransportionActivity onCreate procedure
+		super.onCreate(savedInstanceState);
+		// set the layout to whichever layout this activity is attached to
+		setFrameView(R.layout.activity_friends);
+		
+		// do normal initialization stuff
+		Button testButton = (Button) findViewById(R.id.testButton);
+		testButton.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View arg0) {
+				TextView title = (TextView) findViewById(R.id.title);
+				title.setText("blah blah blah");
+			}
+		});
+	}
+}
