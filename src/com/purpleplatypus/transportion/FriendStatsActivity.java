4f7570ee9565b41956464f4c5cee9c65470caad3
haotian wang <haotianwang92@gmail.com>
2013-04-08 01:23:42 -0700

    deleted unnecessary buttons

--- src/com/purpleplatypus/transportion/FriendStatsActivity.java
+++ src/com/purpleplatypus/transportion/FriendStatsActivity.java
@@ -53,7 +53,6 @@ public class FriendStatsActivity extends Activity {
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.friend_stats, menu);
 		return true;
 	}
 
