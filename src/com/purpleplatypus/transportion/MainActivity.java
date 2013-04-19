7eb21d58ffe8b35459af0186736fdf9940a883f0
Tiffany Lee <tiffanylee@Tiffany-Lees-MacBook.local>
2013-04-19 16:16:21 -0700

    added model and applicationState class along with ui changes to details (tabs and fixed the info to look less like buttons)

--- src/com/purpleplatypus/transportion/MainActivity.java
+++ src/com/purpleplatypus/transportion/MainActivity.java
@@ -1,9 +1,17 @@
+
 package com.purpleplatypus.transportion;
+
+import java.sql.Timestamp;
 
 import org.achartengine.ChartFactory;
 import org.achartengine.model.CategorySeries;
 import org.achartengine.renderer.DefaultRenderer;
 import org.achartengine.renderer.SimpleSeriesRenderer;
+import org.json.JSONException;
+
+import com.parse.Parse;
+import com.parse.ParseAnalytics;
+import com.parse.ParseObject;
 
 import ws.munday.slidingmenu.SlidingMenuActivity;
 import android.os.Bundle;
@@ -23,12 +31,65 @@ public class MainActivity extends TransportionActivity {
 	RelativeLayout pieChartLayout;
 	DrawPieChart pieChart;
 	ChartSection[]chartValues = new ChartSection[4];
+	ApplicationState appState;
 	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setFrameView(R.layout.activity_main);
 
+		// SET UP MODEL
+		appState = (ApplicationState) this.getApplication();
+		appState.data.createDatabase(this);		
+
+		Parse.initialize(this, "i4mqhdigRXwjs66dfZdCdMsF7fuwcIsEGoJUV0Te", "IYX3qei450z9etih7tz7dsobEaenaQmt5oJWu7QT");
+		// track statistics around application opens
+		ParseAnalytics.trackAppOpened(getIntent());
+		
+		// MODEL TESTING - REMOVE
+		/*
+		appState.data.mDbHelper.retrievedDataAddEntry("Car", "Week", (float)123.3, (float)123.3, 123, (float)123.3, (float)123.3);
+		appState.data.mDbHelper.retrievedDataGetEntry("Car", "Week"); // will print
+		appState.data.mDbHelper.retrievedDataRemoveAll();
+		//appState.data.mDbHelper.retrievedDataGetEntry("Car", "Week"); // this causes a problem w/ the cursor b/c its empty => FIX!!!!
+		
+		
+		//ParseObject testObject = new ParseObject("TestObject");
+		//testObject.put("foo", "bar");
+		//testObject.saveInBackground();
+		
+		System.out.println("================= RAW DATA =================");
+		
+		appState.data.mDbHelper.rawDataAddEntry(new Timestamp(new java.util.Date().getTime()), (float)123.090, (float)134.234);
+		//appState.data.mDbHelper.rawDataAddEntry(new Timestamp(new java.util.Date().getTime()), (float)100.111, (float)100.111);
+		//appState.data.mDbHelper.rawDataGetAll(); // will print
+		//appState.data.mDbHelper.rawDataRemoveAll();
+		//appState.data.mDbHelper.rawDataGetAll(); // will print
+		try {
+			appState.data.sendDataToServer();
+		} catch (JSONException e) {	
+			System.out.println("DIDN'T SEND!!!");
+			e.printStackTrace();
+		}
+		
+		
+		appState.data.mDbHelper.rawDataAddEntry(new Timestamp(new java.util.Date().getTime()), (float)111.111, (float)111.111);
+		appState.data.mDbHelper.rawDataAddEntry(new Timestamp(new java.util.Date().getTime()), (float)121.111, (float)111.111);
+		appState.data.mDbHelper.rawDataAddEntry(new Timestamp(new java.util.Date().getTime()), (float)131.111, (float)111.111);
+		appState.data.mDbHelper.rawDataAddEntry(new Timestamp(new java.util.Date().getTime()), (float)141.111, (float)111.111);
+		try {
+			appState.data.sendDataToServer();
+		} catch (JSONException e) {	
+			System.out.println("DIDN'T SEND!!!");
+			e.printStackTrace();
+		}
+		
+		// appState.data.retrieveUserDataFromServer();
+		*/
+		// MODEL TESTING - REMOVE
+		
+		
+		// SET UP REST OF PAGE
 		TextView title = (TextView) findViewById(R.id.title);		
 		title.setText("Overall Usage");
 		
